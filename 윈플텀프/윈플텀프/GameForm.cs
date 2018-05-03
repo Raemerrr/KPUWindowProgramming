6ad1b610bd8d487560f33c7bb8dd5797d594bb6a
Raemin KANG <foals18@gmail.com>
2018-05-03 10:53:17 +0900

    스켈레톤 업로드

---
+++ 윈플텀프/윈플텀프/GameForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 윈플텀프
+{
+    public partial class GameForm : Form
+    {
+        public GameForm()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            titleLabel.Visible = false;
+            startButton.Visible = false;
+        }
+    }
+}
